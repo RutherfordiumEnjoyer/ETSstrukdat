a63ce02d4c4f95713d9fe26a0a424506b1cd9702
AbyanR <ranuajiabyan@gmail.com>
2025-04-20 12:14:45 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <conio.h>
+
 using namespace std;
+
+enum FormatType {
+    NORMAL = 0,
+    BOLD = 1,
+    ITALIC = 2,
+    UNDERLINE = 4
+};
+
+struct TextUnit {
+    char character;
+    int format;
+};
+
+class TextEditor {
+public:
+    TextEditor() {
+    }
+    
+    void run() {
+        cout << "Text Editor sedang berjalan.." << endl;
+        cout << "Tekan tombol apapun untuk keluar" << endl;
+        _getch();
+    }
+};
+
+int main() {
+    TextEditor editor;
+    editor.run();
+    return 0;
+}
