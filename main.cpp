d3c7a41a240091db536e18f6b28b78ccde69e4a9
AbyanR <ranuajiabyan@gmail.com>
2025-04-20 13:06:11 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <conio.h>
-
+#include <string>
+#include <stack>
 using namespace std;
 
 enum FormatType {
@@ -16,14 +17,79 @@ struct TextUnit {
 };
 
 class TextEditor {
+private:
+    string text;
+    int cursorPos;
+    stack<string> undoStack;
+    stack<string> redoStack;
+    
+    void saveState() {
+        undoStack.push(text);
+        while (!redoStack.empty()) redoStack.pop();
+    }
+    
 public:
-    TextEditor() {
+    TextEditor() : cursorPos(0) {}
+    
+    void insertChar(char c) {
+        saveState();
+        text.insert(cursorPos++, 1, c);
+        display();
+    }
+    
+    void deleteChar() {
+        if (cursorPos > 0) {
+            saveState();
+            text.erase(--cursorPos, 1);
+            display();
+        }
+    }
+    
+    void undo() {
+        if (!undoStack.empty()) {
+            redoStack.push(text);
+            text = undoStack.top();
+            undoStack.pop();
+            if (cursorPos > text.length()) cursorPos = text.length();
+            display();
+        }
+    }
+    
+    void redo() {
+        if (!redoStack.empty()) {
+            undoStack.push(text);
+            text = redoStack.top();
+            redoStack.pop();
+            if (cursorPos > text.length()) cursorPos = text.length();
+            display();
+        }
+    }
+    
+    void display() {
+        system("cls");
+        cout << "===== Simple Text Editor =====" << endl;
+        cout << "Text: " << text << endl;
+        
+        cout << "      ";
+        for (int i = 0; i < cursorPos; i++) cout << " ";
+        cout << "^" << endl << endl;
+        
+        cout << "Commands:" << endl;
+        cout << "  Backspace: Hapus | Ctrl+Z: Undo | Ctrl+Y: Redo | Esc: Keluar" << endl;
     }
     
     void run() {
-        cout << "Text Editor sedang berjalan.." << endl;
-        cout << "Tekan tombol apapun untuk keluar" << endl;
-        _getch();
+        display();
+        bool running = true;
+        while (running) {
+            int input = _getch();
+            
+            if (input == 27) running = false;
+            else if (input == 8) deleteChar();
+            else if (input == 26) undo();
+            else if (input == 25) redo();
+            else if (input >= 32 && input <= 126) insertChar((char)input);
+        }
     }
 };
 
